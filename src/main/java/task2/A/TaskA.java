3dc3f74d35db8c24cab034de674504a91fd41f20
rekerok <rekerok05@gmail.com>
2021-08-08 01:52:43 +0300

    made task2a

--- src/main/java/task2/A/TaskA.java
+++ src/main/java/task2/A/TaskA.java
@@ -1,9 +1,25 @@
 package task2.A;
 
+import Variables.Variables;
+
+import java.util.Arrays;
 import java.util.Stack;
+import java.util.stream.Stream;
 
 public class TaskA {
     public static void main(String[] args) {
+        StringBuilder str = new StringBuilder(Variables.faker.number().digits(Variables.faker.number().numberBetween(2, 10)));
+        System.out.println(str);
         Stack<Integer> stack = new Stack<>();
+        for (char symbol : str.toString().toCharArray()) {
+            stack.push(Character.getNumericValue(symbol));
+            System.out.println(stack.peek());
+        }
+        str.setLength(0);
+        System.out.println(stack);
+        stack.stream().forEach(digit -> str.insert(0, digit));
+        // stack.stream().forEach(str::append);
+        System.out.println(str);
     }
+
 }
